d36b92d737ecd6725cca855d64aa371deb6908b1
overfloat <rillrate@gmail.com>
2020-12-17 20:32:40 +0300

    Add CounterProvider

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,32 @@
 use pyo3::prelude::*;
-use rill::prelude::{EntryId, LogProvider as RillLogProvider, Path};
+use rill::prelude::{EntryId, Path};
+use rill::providers;
+
+#[pymodule]
+fn rill(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    // TODO: Return error here
+    rill::install("python").unwrap();
+    m.add_class::<LogProvider>()?;
+    m.add_class::<CounterProvider>()?;
+    Ok(())
+}
+
+fn make_path(entries: Vec<String>) -> Path {
+    let entries: Vec<_> = entries.into_iter().map(EntryId::from).collect();
+    Path::from(entries)
+}
 
 #[pyclass]
 pub struct LogProvider {
-    provider: RillLogProvider,
+    provider: providers::LogProvider,
 }
 
 #[pymethods]
 impl LogProvider {
     #[new]
     fn new(entries: Vec<String>) -> Self {
-        let entries: Vec<_> = entries.into_iter().map(EntryId::from).collect();
-        let path = Path::from(entries);
-        let provider = RillLogProvider::new(path);
+        let path = make_path(entries);
+        let provider = providers::LogProvider::new(path);
         Self { provider }
     }
 
@@ -25,10 +39,25 @@ impl LogProvider {
     }
 }
 
-#[pymodule]
-fn rill(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    // TODO: Return error here
-    rill::install("python").unwrap();
-    m.add_class::<LogProvider>()?;
-    Ok(())
+#[pyclass]
+pub struct CounterProvider {
+    provider: providers::CounterProvider,
+}
+
+#[pymethods]
+impl CounterProvider {
+    #[new]
+    fn new(entries: Vec<String>) -> Self {
+        let path = make_path(entries);
+        let provider = providers::CounterProvider::new(path);
+        Self { provider }
+    }
+
+    fn is_active(&mut self, _py: Python) -> bool {
+        self.provider.is_active()
+    }
+
+    fn inc(&mut self, _py: Python, delta: u64) {
+        self.provider.inc(delta, None);
+    }
 }
