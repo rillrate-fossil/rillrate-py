ae0151a49bf0644bc6ddfc4650daea667cb4b2c6
overfloat <rillrate@gmail.com>
2021-03-21 18:28:42 +0300

    Add simple-test.py

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-cargo build
+cargo build --offline
+cp target/debug/librillrate.so rillrate/rillrate.so
 
+exit
+
+cargo build
 pushd target/debug
 mv librillrate.so rillrate.so
 mv librillrate.dylib rillrate.so
