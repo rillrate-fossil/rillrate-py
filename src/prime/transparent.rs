74b486974cc4d48f9b07b0ef4476c43cb043f86d
Denis Kolodin <deniskolodin@gmail.com>
2021-08-29 12:51:40 +0300

    Add Alert tracer

--- src/prime/transparent.rs
+++ src/prime/transparent.rs
@@ -1,5 +1,26 @@
 use pyo3::prelude::*;
+use rillrate::prime;
 
-pub fn init(_py: Python<'_>, _m: &PyModule) -> PyResult<()> {
+pub fn init(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Alert>()?;
     Ok(())
+}
+
+#[pyclass]
+pub struct Alert {
+    tracer: prime::Alert,
+}
+
+#[pymethods]
+impl Alert {
+    #[new]
+    fn new(path: String) -> Self {
+        let opts = prime::AlertOpts {};
+        let tracer = prime::Alert::new(path, opts);
+        Self { tracer }
+    }
+
+    fn notify(&mut self, message: String) {
+        self.tracer.notify(message);
+    }
 }
