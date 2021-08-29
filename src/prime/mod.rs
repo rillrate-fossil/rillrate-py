b7262278d3350c1a4a790f2a2da82de64a8433ee
Denis Kolodin <deniskolodin@gmail.com>
2021-08-29 12:46:17 +0300

    Add layers

--- src/prime/mod.rs
+++ src/prime/mod.rs
@@ -1,13 +1,15 @@
-mod controls;
-mod flows;
+mod control;
+mod transparent;
+mod visual;
 
 use pyo3::prelude::*;
 
 pub fn register(py: Python, parent_module: &PyModule) -> PyResult<()> {
     let submodule = PyModule::new(py, "prime")?;
 
-    controls::init(py, submodule)?;
-    flows::init(py, submodule)?;
+    control::init(py, submodule)?;
+    transparent::init(py, submodule)?;
+    visual::init(py, submodule)?;
 
     parent_module.add_submodule(submodule)?;
 
---
+++ src/prime/transparent.rs
@@ -0,0 +1,5 @@
+use pyo3::prelude::*;
+
+pub fn init(_py: Python<'_>, _m: &PyModule) -> PyResult<()> {
+    Ok(())
+}
